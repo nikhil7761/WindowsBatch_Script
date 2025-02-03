766a893d5121ae56df7d6afcd0847591ee0d0ba2
nikhil.vargude <nikhilvargude@gmail.com>
2025-02-03 22:30:42 +0530

    added files

---
+++ Array.cmd
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+SET List=1 8 3 5 4 3 3 2 4 5 6 64 43 2 144 4
+(for %%P in (%List%) do (
+    echo %%P
+))
+
+
+
+Set number=1,2,3,3,4,6,4,32,2,13,5,6,6,7775,53,3
+(for %%n in (%number%) do (
+
+    echo %%n
+))
